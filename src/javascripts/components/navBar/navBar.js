50fd369a8d00a41f68a01da14f4919b9ec61cb8b
Michael Baumer <meesterbaumer@mac.com>
2019-10-19 13:48:27 -0500

    Working Nav

--- src/javascripts/components/navBar/navBar.js
+++ src/javascripts/components/navBar/navBar.js
@@ -5,7 +5,9 @@ import './navBar.scss';
 const loadNavbar = () => {
   const domString = `
   <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Chatty</a>
+      <a class="navbar-brand" href="#">
+      <img src="src/img/ChatWorld.png" height="100" alt="">
+      </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
