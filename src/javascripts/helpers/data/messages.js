05362e647b93f6eae15c61fb872216c361ae1e4a
Monica Djunaidi <modjun12@gmail.com>
2019-10-20 16:24:46 -0500

    updated lines of code

--- src/javascripts/helpers/data/messages.js
+++ src/javascripts/helpers/data/messages.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 const chats = [
   {
     message: 'mom',
@@ -20,28 +18,4 @@ const chats = [
 
 const getChats = () => chats;
 
-const addChat = (obj) => {
-  chats.push(obj);
-  if (chats.length > 20) {
-    chats.shift();
-  }
-};
-
-const deleteClick = (e) => {
-  const deleteChat = $(e.target).parents('.card');
-  for (let k = 0; k < chats.length; k += 1) {
-    if (chats[k].message === $(deleteChat).find('p').html()) {
-      chats.splice(k, 1);
-    }
-  }
-};
-
-const deleteEvent = () => {
-  const deleteButtons = $('.d-flex').children('.delete');
-  for (let j = 0; j < deleteButtons.length; j += 1) {
-    const singleDeleteButton = deleteButtons[j];
-    $(singleDeleteButton).click(deleteClick);
-  }
-};
-
-export default { getChats, addChat, deleteEvent };
+export default { getChats };
