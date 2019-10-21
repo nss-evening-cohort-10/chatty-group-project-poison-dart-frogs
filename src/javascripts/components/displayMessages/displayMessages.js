4f80d437833f663dcb2b5e253bf1baa0531ca2c5
Monica Djunaidi <modjun12@gmail.com>
2019-10-21 18:31:39 -0500

    cleaned up timestamp

--- src/javascripts/components/displayMessages/displayMessages.js
+++ src/javascripts/components/displayMessages/displayMessages.js
@@ -1,9 +1,8 @@
 import $ from 'jquery';
+import moment from 'moment';
 import chats from '../../helpers/data/messages';
 import utilities from '../../helpers/utilities';
 import './displayMessages.scss';
-
-const nowDate = new Date();
 
 const messagesPrint = () => {
   let messageString = '<div class="row">';
@@ -16,7 +15,7 @@ const messagesPrint = () => {
         <div class="card-body">
           <p class="card-text">${message.message}</p>
           <div class="d-flex justify-content-between">
-            <p>${nowDate}</p>
+            <p>${moment().format('lll')}</p>
             <button class="btn btn-primary delete">Delete</button>
           </div>  
         </div>
