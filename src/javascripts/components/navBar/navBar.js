b9a69e47b6c16258d274628fb618ebfabef8f09a
Ivan Phelps <ivanniothemannio@gmail.com>
2019-10-21 21:43:50 -0500

    added fun stuff

--- src/javascripts/components/navBar/navBar.js
+++ src/javascripts/components/navBar/navBar.js
@@ -5,7 +5,7 @@ import './navBar.scss';
 const loadNavbar = () => {
   const domString = `
   <nav class="fixed-top navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href=".">
       <img src="src/img/ChatWorld.png" height="100" alt="">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
